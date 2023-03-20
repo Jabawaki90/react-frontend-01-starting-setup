692e6a58338b60ee6ef93ae891450bca21f1d312
Ashraf Azemi <ashraf.chzm@gmail.com>
2023-03-20 08:42:11 +0800

    MERN - frontend setup

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch,
+} from "react-router-dom";
+import Users from "./user/pages/Users";
+import NewPlaces from "./places/pages/NewPlaces";
+
+function App() {
+  return (
+    <Router>
+      <Switch>
+        <Route path="/" exact>
+          <Users />
+        </Route>
+        <Route path="/places/new">
+          <NewPlaces />
+        </Route>
+        <Redirect path="/" />
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
