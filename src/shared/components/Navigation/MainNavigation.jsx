50f9c04c4091fcb7c696557b49ca0877d53158ed
Ashraf Azemi <ashraf.chzm@gmail.com>
2023-03-21 09:54:47 +0800

    animating side drawers

--- src/shared/components/Navigation/MainNavigation.jsx
+++ src/shared/components/Navigation/MainNavigation.jsx
@@ -1,24 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import MainHeader from "./MainHeader";
 import NavLinks from "./NavLinks";
 import SideDrawer from "./SideDrawer";
+import Backdrop from "../UIElement/Backdrop";
 import { Link } from "react-router-dom";
 
 import "./MainNavigation.css";
 
 const MainNavigation = (props) => {
+  const [sideBarToggle, setSideBarToggle] = useState(false);
   return (
     <>
-      {false && (
-        <SideDrawer>
-          <nav className="main-navigation__drawer-nav">
-            <NavLinks />
-          </nav>
-        </SideDrawer>
+      {sideBarToggle && (
+        <Backdrop onClick={() => setSideBarToggle(!sideBarToggle)} />
       )}
 
+      <SideDrawer
+        show={sideBarToggle}
+        onClick={() => setSideBarToggle(!sideBarToggle)}
+      >
+        <nav className="main-navigation__drawer-nav">
+          <NavLinks />
+        </nav>
+      </SideDrawer>
+
       <MainHeader>
-        <button className="main-navigation__menu-btn">
+        <button
+          className="main-navigation__menu-btn"
+          onClick={() => setSideBarToggle(!sideBarToggle)}
+        >
           <span />
           <span />
           <span />
--- src/user/pages/Users.jsx
+++ src/user/pages/Users.jsx
@@ -4,20 +4,20 @@ import UsersList from "../components/UsersList";
 
 const DUMMY_DATA = [
   {
-    id: 1,
+    id: "u1",
     image:
       "https://upload.wikimedia.org/wikipedia/commons/3/3c/Vue_de_nuit_de_la_Place_Stanislas_%C3%A0_Nancy.jpg",
     name: "france",
     places: 3,
   },
   {
-    id: 2,
+    id: "u2",
     image: "https://study.com/cimages/multimages/16/eiffel_tower.jpg",
     name: "paris",
     places: 8,
   },
   {
-    id: 3,
+    id: "u3",
     image:
       "https://www.planetware.com/wpimages/2019/10/malaysia-best-places-to-visit-kuala-lumpur.jpg",
     name: "Kuala Lumpur",
